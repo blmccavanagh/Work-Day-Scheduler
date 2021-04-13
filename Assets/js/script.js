fa22c2c8e57d457d52ad8f93c864bb18dad90072
blmccavanagh <blmccavanagh@gmail.com>
2021-04-14 03:09:31 +0800

    Color coding function added. JS file updated to include color coding function. HTML updated to reflect relevant IDs used in JS file.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
 });
 
 // Function to update time
-setInterval(updateTimeSensitiveFunctions, 1000);
+setInterval(updateTimeSensitiveFunctions, 1000)
 
 // Function to update scheduler based on current time
 function updateTimeSensitiveFunctions(){
 
     displayCurrentDateAndTime();
-    // colorCode();
+    colorCode();
 
 }
 
@@ -27,17 +27,29 @@ function displayCurrentDateAndTime(){
 // Function to color code scheduler based on time
 // Add background color to hours of the day
 // Grey if hour has passed
+// - add class .past
 // Red if current hour
+// - add class .present
 // Green for future hours
+// - add class .future
 
 function colorCode(){
     // Convert string value of hour to number value
     let currentTime = parseInt(now.format('h'));
 // Use the value from now.format
 // Create for loop
-// if current time then red
-// if less than current time then grey
-// if more than current time then green
+// if less than current time .past
+// if current time .present
+// if more than current time .future
+for (let i = 8; i <= 18; i++) {
+    if (i < currentTime) {
+        $('#' + i).addClass("past");
+    } else if (i === currentTime) {
+        $('#' + i).addClass("present");
+    } else {
+        $('#' + i).addClass("future");
+    }
+}
 }
 
 // Function to display schedule
